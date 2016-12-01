3a8b8c14087acedd878bae24c474871637f5b996
Shinmera <shinmera@tymoon.eu>
2016-12-01 23:23:19 +0100

    Installer should actually work now.

--- install.sh
+++ install.sh
@@ -4,12 +4,6 @@ readonly SHIRAKUMO_URL="http://dist.tymoon.eu/shirakumo.txt"
 readonly DEFAULT_TARGET=~/autobuild
 readonly DEFAULT_DOMAINS="radiance localhost"
 
-readonly QUICKLISP_FILE="${target}/quicklisp.lisp"
-readonly QUICKLISP_DIR="${target}/quicklisp/"
-readonly AUTOBUILD_DIR="${target}/projects/"
-readonly CONFIG_DIR="${target}/config/"
-readonly STARTUP_FILE="${target}/autobuild"
-
 function sbcl-eval(){
     sbcl --noinform \
          --disable-ldb \
@@ -19,33 +13,62 @@ function sbcl-eval(){
          --no-sysinit \
          --noprint \
          --load "${QUICKLISP_DIR}setup.lisp" \
-         --eval "(progn $@)" \
+         --eval "(progn $*)" \
          --quit
 }
 
+function eexit(){
+    echo "$(tput setaf 1) ! Error: $(tput sgr 0)" $@
+    rm -r "${target:-/dev/null}"
+    exit 1
+}
+
+function status() {
+    local level=$1
+    case "$level" in
+        0) echo "$(tput setaf 2) ==> $(tput sgr 0)" ${@:2};;
+        1) echo "$(tput setaf 3)   -> $(tput sgr 0)" ${@:2};;
+        *) echo "$(tput setaf 6)     > $(tput sgr 0)" ${@:2};;
+    esac
+}
+
 function configure(){
-    read -p "Enter the installation target [${DEFAULT_TARGET}] " target
+    status 1 "Configuring setup"
+    read -p "Enter the installation directory [${DEFAULT_TARGET}] " target
     target=${target:-"${DEFAULT_TARGET}"}
 
-    read -p "Enter the list of domains [${DEFAULT_DOMAINS}]" domains
+    read -p "Enter the list of domains for this machine [${DEFAULT_DOMAINS}] " domains
     domains=${domains:-"${DEFAULT_DOMAINS}"}
+
+    readonly QUICKLISP_FILE="${target}/quicklisp.lisp"
+    readonly QUICKLISP_DIR="${target}/quicklisp/"
+    readonly AUTOBUILD_DIR="${target}/projects/"
+    readonly CONFIG_DIR="${target}/config/"
+    readonly STARTUP_FILE="${target}/autobuild"
+    LISP_DOMAINS=""
+    for domain in $domains; do
+        LISP_DOMAINS="$LISP_DOMAINS \"$domain\""
+    done
 }
 
 function install-quicklisp(){
+    status 1 "Installing Quicklisp"
     mkdir -p "${QUICKLISP_DIR}"
     mkdir -p "${AUTOBUILD_DIR}"
-    mkdir -p "${CONFIG_DIR}"
     
-    curl -o "${QUICKLISP_FILE}" "${QUICKLISP_URL}"
+    curl -o "${QUICKLISP_FILE}" "${QUICKLISP_URL}" \
+        || eexit "Failed to download Quicklisp file."
     sbcl --no-userinit \
          --load "$QUICKLISP_FILE" \
          --eval "(quicklisp-quickstart:install :path \"${QUICKLISP_DIR}\")" \
          --eval "(ql-dist:install-dist \"${SHIRAKUMO_URL}\" :prompt NIL)" \
-         --eval "(sb-ext:quit)"
+         --eval "(sb-ext:quit)" \
+        || eexit "Failed to install Quicklisp"
 }
 
 function write-radiance-config(){
-        cat >"$1" <<EOF
+    mkdir -p $(dirname "$1")
+    cat >"$1" <<EOF
 ((:interfaces
   (:logger . "i-verbose")
   (:data-model . "r-simple-model")
@@ -55,14 +78,14 @@ function write-radiance-config(){
   (:session . "r-simple-sessions")
   (:server . "i-hunchentoot"))
  (:server
-  (:domains . ("radiance" "localhost" "127.0.0.1" "::1"))
+  (:domains . ($LISP_DOMAINS))
   (:instances . (((:port . 2015)))))
  (:startup :r-simple-errors :autobuild-server))
 EOF
 }
 
 function write-radiance-launcher(){
-        cat >"$1" <<EOF
+    cat >"$1" <<EOF
 #!/bin/sh
 sbcl --no-userinit \
      --no-sysinit \
@@ -77,24 +100,26 @@ EOF
 }
 
 function install-autobuild(){
-    sbcl-eval "(ql-dist:install-dist \"${SHIRAKUMO_URL}\" :prompt NIL)" \
-              "(ql:quickload :radiance)" \
-              "(ql:quickload :autobuild-server)"
-    
-    write-radiance-config "${CONFIG_DIR}/default/radiance-core/radiance.core.conf.lisp"
+    status 1 "Installing Autobuild"
+    write-radiance-config "${CONFIG_DIR}/default/radiance-core/radiance-core.conf.lisp"
     write-radiance-launcher "${STARTUP_FILE}"
 
     (exec "${STARTUP_FILE}" \
           --eval "(radiance:shutdown)" \
-          --eval "(sb-ext:quit)")
+          --eval "(sb-ext:quit)") \
+        || eexit "Failed to setup autobuild"
 }
 
 function main(){
+    status 0 "Autobuild installer"
+    
     configure
     install-quicklisp
     install-autobuild
 
     echo ""
-    echo "Autobuild installed in ${target}."
-    echo "Run ${STARTUP_FILE} to launch autobuild."
+    status 2 "Autobuild installed in ${target}."
+    status 2 "Run ${STARTUP_FILE} to launch autobuild."
 }
+
+main
